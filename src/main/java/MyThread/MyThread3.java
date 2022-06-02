4974f4b25c2d9a1f152ec68b6ec007036f03bbaa
wenjianli3 <wenjianli3@jd.com>
2022-06-02 10:03:43 +0800

    多线程-synchronized解决线程安全问题

--- src/main/java/MyThread/MyThread3.java
+++ src/main/java/MyThread/MyThread3.java
@@ -8,7 +8,7 @@ public class MyThread3 extends Thread{
     private int count = 5;
 
     @Override
-    public void run() {
+    synchronized public void run() {
         super.run();
         count--;
         System.out.println("由"+ Thread.currentThread().getName() + "计算，count="+ count);
